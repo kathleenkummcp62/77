08930f1a6dab38ef07e0b00cda4e80954b2edf6a
kathleenkummcp62 <160126900+kathleenkummcp62@users.noreply.github.com>
2025-06-29 12:59:16 -0700

    fix: ensure lint works on Node 22

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import js from '@eslint/js';
 import { fileURLToPath } from 'url';
 import path from 'path';
-import eslintrc from './.eslintrc.json' assert { type: 'json' };
+import eslintrc from './.eslintrc.json' with { type: 'json' };
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
